1c687446d9d6c19fbd2f6e64cafcf73caa505f51
Mahesh Kumar <maheshkumar.wdp21@gmail.com>
2016-02-16 07:59:35 +0530

    Added index file

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template
+from flask import request, redirect, url_for
+from werkzeug import secure_filename
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
-	msg = "Welcome to the home page"
-	return render_template("index.html", msg=msg)
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template("index.html")
 
+# Image upload
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# 404 error handler
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
+    
 if __name__ == '__main__':
 	app.run(debug = True)
